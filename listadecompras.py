4c517e2f804bdebf4b06562f3e529658160fba41
danielapsantos <daniela.rps09@gmail.com>
2024-09-03 22:20:18 -0300

    Função para adicionar produto e listar

--- listadecompras.py
+++ listadecompras.py
@@ -1,5 +1,10 @@
 def listadecompras():
+
+    lista = []
+
     while True:
+        listar_produtos(lista)
+        print()
         print("Lista de Compras Simples")
         print()
         print("1 - Adicionar produto")
@@ -11,6 +16,57 @@ def listadecompras():
         if selecionar == 's' or selecionar == 'S':
             print("Obrigada por utilizar a Lista de Compras.")
             break
-        if selecionar not in ['1','2','3']:
+
+        if selecionar == "1":
+            lista.append(adicionar_produto())
+        elif selecionar == "2":
+            # remover_produto()
+            print("remover")
+        elif selecionar == "3":
+            print("pesquisar")
+            # pesquisar_produto()
+        elif selecionar not in ['1','2','3']:
             print("Opção inválida! Tente novamente!")
             continue
+            
+
+def adicionar_produto():
+    nome = input("Digite o nome do item:")
+    
+    print("Qual a unidade de medida?")
+    print("1 - Quilograma")
+    print("2 - Grama")
+    print("3 - Mililitro")
+    print("4 - Unidade")
+    print("5 - Metro")
+    print("6 - Centímetro")
+    opcao = input("Digite a opção:")
+    while opcao not in ['1','2','3','4','5','6']:
+        if opcao not in ['1','2','3','4','5','6']:
+            print("Opção inválida! Tente novamente!")
+            opcao = input("Digite a opção:")
+
+    if opcao == "1":
+        unidade_medida = "Quilograma"
+    elif opcao == "2":
+        unidade_medida = "Grama"
+    elif opcao == "3":
+        unidade_medida = "Mililitro"
+    elif opcao == "4":
+        unidade_medida = "Unidade"
+    elif opcao == "5":
+        unidade_medida = "Metro"
+    elif opcao == "6":
+        unidade_medida = "Centímetro"
+         
+    quantidade = input("Digite a quantidade:")
+    descricao = input("Digite a descrição:")
+
+    return {"Nome":nome, "Unidade":unidade_medida, "Quantidade":quantidade, "Descrição":descricao}
+
+def listar_produtos(lista):
+    for i in range(len(lista)):
+        print(lista[i])
+
+
+listadecompras()
